059b2283fc2d0b348cb264dbfb06b1e7dbfa1fd8
caiolamoglia1 <lamogliacarrer@gmail.com>
2025-04-21 20:25:20 -0300

    sessão back

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import mysql from "mysql2";
 import bcrypt from 'bcryptjs';
+import session from 'express-session';
 
 const app = express(); 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
 app.use(express.json());
 
 const db = mysql.createConnection({
@@ -13,6 +18,18 @@ const db = mysql.createConnection({
     password: "PUC@1234",
     database: "orb"
 })
+
+app.use(session({
+    secret: 'ola',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}));
+
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
@@ -24,15 +41,19 @@ app.get("/api/Ola", (req, res) => {
 
 app.get("/api/usuario", (req, res) => {
     const q = "SELECT * FROM usuario"
+    
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.json(data)
     })
 })
+
+
 app.post("/api/usuario", async (req, res) => {
-    const q = "INSERT INTO usuario (`email`, `curso`, `turno`, `dataNasc`, `senha`) VALUES (?)"
+    const q = "INSERT INTO usuario (`nome`,`email`, `curso`, `turno`, `dataNasc`, `senha`) VALUES (?)"
     const hashPassword = await bcrypt.hash(req.body.senha, 10)
     const values = [
+        req.body.nome,
         req.body.email,
         req.body.curso,
         req.body.turno,
@@ -46,22 +67,36 @@ app.post("/api/usuario", async (req, res) => {
 })
 
 app.post("/api/login", async (req, res) => {
-    const { email, senha } = req.body;
- 
-    const q =` SELECT * FROM usuario WHERE email = ?`
- 
-    db.query(q, [email], async (err, data) => {
-     if (err) return res.status(500).json(err)
- 
-     if(data.length === 0) return res.status(401).json({message: 'Usuário não encontrado'})
- 
-     const usuario = data[0];
-     const passmatch = await bcrypt.compare(senha, usuario.senha);
- 
-     if (!passmatch) {
-         return res.status(401).json({message: 'Senha incorreta.'})
-     }
- 
-     return res.status(200).json({message: "Logado com sucesso."})
-    })
- })
+   const { email, senha } = req.body;
+
+   const q = `SELECT * FROM usuario WHERE email = ?`
+
+   db.query(q, [email], async (err, data) => {
+    if (err) return res.status(500).json(err)
+
+    if(data.length === 0) return res.status(401).json({message: 'Usuário não encontrado'})
+
+    const usuario = data[0];
+    const passmatch = await bcrypt.compare(senha, usuario.senha);
+
+    if (!passmatch) {
+        return res.status(401).json({message: 'Senha incorreta.'})
+    }
+
+    req.session.usuario = {
+        id: usuario.id,
+        email: usuario.email,
+        nome: usuario.nome
+    }
+
+    return res.status(200).json({message: "Logado com sucesso."})
+   })
+})
+
+app.get("/api/session", (req, res) => {
+    if (req.session.usuario) {
+        return res.json(req.session.usuario);
+    } else {
+        return res.status(401).json({ message: "Usuário não autenticado" });
+    }
+});
